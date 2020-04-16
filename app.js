e6bb51933c0b556e89a3f8b53fa2a29abd08a3b5
albertogalan <alberto.galanfernandez.ext@lafargeholcim.com>
2020-04-16 10:42:34 +0200

    fix feature detected #1

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello, World!\n');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+PORT=8085
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT} !`);
 });
+
 
 module.exports = app;
